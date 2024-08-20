9b2905d36879caf7fe9555d4dfab40d118eae0aa
jvm2023 <125207795+jvm2023@users.noreply.github.com>
2024-08-21 00:27:33 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,18 +1,1 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.78.0"
-    }
-  }
-}
-
-provider "google" {
-  # Configuration options
-
-
-  
-
-
-}
 
