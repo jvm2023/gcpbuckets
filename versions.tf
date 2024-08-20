5a7931e7869278296ed37809ea95c5073cef7cc4
jvm <cloudgraph123@gmail.com>
2024-08-20 22:55:18 +0530

    second commit

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.78.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+
+
+  
+
+
+}
+
